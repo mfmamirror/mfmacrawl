57024b5cd307b54b711d88c13e25eae7c751151d
JD Bothma <jbothma@gmail.com>
2016-06-11 08:34:58 +0200

    start spidering along menu

--- mfma/spiders/mfma_spider.py
+++ mfma/spiders/mfma_spider.py
@@ -1,5 +1,6 @@
 import scrapy
 from mfma.items import PageItem, MenuItem
+import urlparse
 
 
 class MfmaSpider(scrapy.Spider):
@@ -10,6 +11,10 @@ class MfmaSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        for result in self.parse_home(response):
+            yield result
+
+    def parse_home(self, response):
         filename = response.url.split("/")[-2] + '.html'
         path = 'scrape/' + filename
         with open(path, 'wb') as f:
@@ -18,18 +23,26 @@ class MfmaSpider(scrapy.Spider):
         menu_item = MenuItem()
         menu_items = []
         for menu_link in response.selector.css('#zz1_QuickLaunchMenu a'):
+            url = menu_link.xpath('@href').extract()[0]
+            if urlparse.urlparse(url).scheme:
+                abs_url = url
+            else:
+                abs_url = urlparse.urljoin(response.url, url)
+            yield scrapy.Request(abs_url, callback=self.parse_page)
             menu_items.append({
-                'href': menu_link.xpath('@href').extract()[0],
+                'url': url,
                 'text': menu_link.xpath('text()').extract()[0],
             })
         menu_item['menu_items'] = menu_items
         yield menu_item
 
         item = PageItem()
-        item['body'] = response.selector.css('.ms-WPBody').extract()
-        # response.selector.xpath("//div[@class='ms-WPBody']")
-
-
-        item['breadcrumbs'] = response.selector.css('#ctl00_PlaceHolderTitleBreadcrumb_siteMapPath').extract()
-        item['title'] = response.selector.css('.breadcrumbCurrent').extract()
+        item['body'] = response.selector.css('.ms-WPBody > P').extract()
+        breadcrumbs_css = '#ctl00_PlaceHolderTitleBreadcrumb_siteMapPath'
+        item['breadcrumbs'] = response.selector.css(breadcrumbs_css).extract()
+        title_css = '.breadcrumbCurrent'
+        item['title'] = response.selector.css(title_css).xpath('text()').extract()
         yield item
+
+    def parse_page(self, response):
+        pass
