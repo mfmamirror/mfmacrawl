75d250f9a5521eca6cb7f6782d2a907da4020690
JD Bothma <jbothma@gmail.com>
2016-09-18 15:52:39 +0200

    Add command-line builder til I figure out an automated builder solution

--- mfma/pipelines.py
+++ mfma/pipelines.py
@@ -7,14 +7,9 @@ This is quite specific to the site being mirrored but a lot of cool stuff can be
 be learned from this to mirror other sites.
 """
 
-import json
-import os
-import codecs
-import yaml
-import re
+from builder import Builder
 from git import Repo
 from git import Actor
-from git.exc import InvalidGitRepositoryError
 from scrapy.utils.project import data_path
 import logging
 import os
@@ -65,18 +60,14 @@ class MirrorBuilderPipeline(object):
         self.mirror.upload()
 
     def process_item(self, item, spider):
-        if item['type'] == 'page':
-            self.mirror.write_page(item)
-        elif item['type'] == 'menu':
-            self.mirror.make_menu(item)
-
+        self.mirror.builder_.handle_item(item)
         return item
 
 
 class MFMAMirror(object):
 
     def __init__(self, uri, path):
-        self.path = path
+        self.builder_ = Builder(path)
 
         if os.path.exists(self.path):
             self.repo = Repo(self.path)
@@ -102,41 +93,3 @@ class MFMAMirror(object):
         logger.info("pushing changes to mirror")
         self.repo.remotes.origin.push()
         logger.info("done pushing changes to mirror")
-
-    def write_menu(self, menu):
-        jsonstr = json.dumps(menu['menu_items'])
-        self.write_file('_data/menu.json', jsonstr)
-
-    def write_page(self, page):
-        table_items = page['form_table_rows']
-
-        for item in table_items:
-            if self.has_file_extension(item['path']):
-                item['path'] = 'http://mfma.treasury.gov.za' + item['path']
-
-        preamble_data = {
-            'title': page.get('title' ''),
-            'breadcrumbs': page.get('breadcrumbs', ''),
-            'layout': 'default',
-            'original_url': page['original_url'],
-            'table_items': page['form_table_rows'],
-        }
-
-        preamble_yaml = yaml.safe_dump(preamble_data)
-        content = page.get('body', '')
-        pagestr = "---\n%s\n---\n%s" % (preamble_yaml, content)
-        filename = page['path'] + '/index.html'
-        self.write_file(filename, pagestr)
-
-    def write_file(self, filename, data):
-        filename = self.path + filename
-        directory = os.path.dirname(filename)
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-        with codecs.open(filename, 'w', encoding='utf8') as file:
-            file.write(data)
-
-    @staticmethod
-    def has_file_extension(path):
-        regex = '^.+(\..{1,4})$'
-        return re.match(regex, path)
