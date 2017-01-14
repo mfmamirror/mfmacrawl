907087eb607cceb3a2f7a33292645057d56c7712
JD Bothma <jbothma@gmail.com>
2017-01-14 18:19:03 +0200

    disable depaging

--- mfma/settings.py
+++ mfma/settings.py
@@ -65,7 +65,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    'mfma.pipelines.DepagingPipeline': 100,
+#    'mfma.pipelines.DepagingPipeline': 100,
     'mfma.pipelines.FileArchivePipeline': 100,
 #    'mfma.pipelines.MirrorBuilderPipeline': 300,
 }
