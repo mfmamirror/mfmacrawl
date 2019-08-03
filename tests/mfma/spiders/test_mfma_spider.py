61fa695595f46bc2f5e7c4ccf4d0a572d183cd58
JD Bothma <jbothma@gmail.com>
2019-08-03 09:27:43 +0200

    WIP failing test

--- tests/mfma/spiders/test_mfma_spider.py
+++ tests/mfma/spiders/test_mfma_spider.py
@@ -2,7 +2,7 @@ import os
 from unittest import TestCase
 from scrapy.http import HtmlResponse, Request
 from mfma.spiders import mfma_spider
-from mfma.items import MenuItem
+from mfma.items import MenuItem, PageItem
 
 
 class ScrapeMenuTestCase(TestCase):
@@ -41,3 +41,25 @@ class ScrapeMenuTestCase(TestCase):
                 ]
             ),
         )
+
+
+class FormTableContentTestCase(TestCase):
+    def setUp(self):
+        with open(
+            os.path.join(
+                os.path.splitext(__file__)[0],
+                self.__class__.__name__ + "_page_source.html",
+            )
+        ) as page_source_file:
+            page_source = page_source_file.read()
+
+        self.response = HtmlResponse("http://mfma.treasury.gov.za/", body=page_source)
+        self.spider = mfma_spider.MfmaSpider()
+        self.page_item = PageItem()
+        self.page_item["type"] = "page"
+        self.page_item["form_table_rows"] = []
+
+    def test_set_form_table_content(self):
+        items = list(self.spider.set_form_table_content(self.page_item, self.response))
+        self.assertEqual(7, len(self.page_item["form_table_rows"]))
+        self.assertEqual(7, len(items))
